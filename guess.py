34f6d07a4d63e7042ccecdba93f03168f4bc317c
Jan <jan.logar000@gmail.com>
2016-10-20 17:04:33 +0200

    riddle

---
+++ guess.py
@@ -0,0 +1,6 @@
+while True:
+    answer = raw_input("Try and guess a number that is combined with the day and month of my GF's BD. Please enter your number: ")
+    if answer == "38":
+        print("HA You guessed it! Congratulations")
+        break
+    print("Nah, that is not it.")
